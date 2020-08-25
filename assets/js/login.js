c30ccf8c14b442f7aa943eaea92998047986e7a3
zhaojia <1363043290@qq.com>
2020-08-25 21:04:53 +0800

    已完成登录/注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,64 @@
+$(function () {
+    // 登录界面跳转注册界面
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    // 注册界面跳转登录界面
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    });
+    // 表单验证
+    var form = layui.form;
+    form.verify({
+        // 密码框
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 确认密码框
+        repwd: function (value) {
+            // 判断前后输入密码是否相同
+            if ($('.reg-box [name=password]').val() !== value) return '两次密码输入不一致';
+        }
+    });
+    // 发送 Ajax 请求
+    var layer = layui.layer;
+    // 登录框
+    $('#form_login').submit(function (e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                layer.msg('登录成功！');
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token);
+                // 跳转到后台主页
+                location.href = '../../index.html';
+            }
+        })
+    });
+    // 注册框
+    $('#form_reg').on('submit', function (e) {
+        // 阻止表单的默认的提交行为
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/reguser',
+            data: {
+                username: $('#form_reg [name=username]').val(),
+                password: $('#form_reg [name=password]').val()
+            },
+            success: function (res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                layer.msg('注册成功，请登录！');
+                // 模拟人的点击行为
+                $('#link_login').click();
+            }
+        })
+    });
+    
+})
